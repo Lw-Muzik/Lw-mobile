6107f1dafb445da4e79d35f9ee8f7d98a84b5e1e
bruno <brunohectre@gmail.com>
2026-03-20 14:10:59 +0300

    chore: Remove unused imports and apply minor formatting in player settings and stem button widgets.

--- lib/player/widgets/PlayerSettings.dart
+++ lib/player/widgets/PlayerSettings.dart
@@ -1,10 +1,7 @@
-import 'package:eq_app/Routes/routes.dart';
-import 'package:eq_app/widgets/HorizontalSlider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../controllers/AppController.dart';
-import '../../widgets/PlayListWidget.dart';
 
 class PlayerSettings extends StatefulWidget {
   final AppController controller;
@@ -19,27 +16,29 @@ class _PlayerSettingsState extends State<PlayerSettings> {
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.width,
-      child: Consumer<AppController>(builder: (context, control, child) {
-        return ListView(
-          children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Text(
-                  "More Options",
-                  style: Theme.of(context).textTheme.titleLarge,
+      child: Consumer<AppController>(
+        builder: (context, control, child) {
+          return ListView(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    "More Options",
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
                 ),
               ),
-            ),
-            // controls
 
-            ListTile(
-              leading: const Icon(Icons.playlist_add),
-              title: const Text("Add to playlist"),
-            )
-          ],
-        );
-      }),
+              // controls
+              ListTile(
+                leading: const Icon(Icons.playlist_add),
+                title: const Text("Add to playlist"),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
